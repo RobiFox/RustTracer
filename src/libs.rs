542180ccd6d9ca54d67bee99945b402a942d0aa4
Robieh <10639273+RobiFox@users.noreply.github.com>
2024-05-11 14:23:47 +0200

    improved clamping

--- src/libs.rs
+++ src/libs.rs
@@ -2,9 +2,9 @@ use std::f64::consts::PI;
 use crate::vec3::Vec3;
 
 pub fn write_color(color: &Vec3) -> String {
-    let ir = (255.0 * color.x).floor() as u8;
-    let ig = (255.0 * color.y).floor() as u8;
-    let ib = (255.0 * color.z).floor() as u8;
+    let ir = (255.999 * color.x).floor() as u8;
+    let ig = (255.999 * color.y).floor() as u8;
+    let ib = (255.999 * color.z).floor() as u8;
 
     format!("{} {} {}\n", ir, ig, ib)
 }
